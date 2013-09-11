cdef8894617806929463d74abc7db7b4ef23ee3e
Sven Malvik <sven@malvik.de>
2013-09-11 21:48:57 +0200

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cv_service
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cv_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns cv-service.core-test
+  (:use clojure.test
+        cv-service.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
