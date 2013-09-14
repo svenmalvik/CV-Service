b58bf9e0913ecfaa08cd9e4eb77900d8cbf1c112
Sven Malvik <sven@malvik.de>
2013-09-14 21:24:24 +0200

    Measures time spend from incoming request to outgoing request

--- src/cv_service/core.clj
+++ src/cv_service/core.clj
@@ -7,11 +7,21 @@
   (:require [compojure.route :as route]
             [cv_service.me :as me]))
 
+(defmacro bench [expr]
+  `(let [start# (System/nanoTime)
+         ret# ~expr]
+     (println
+       (str
+         "Spend time "
+         '(expr)
+         (- (System/nanoTime) start#)))
+     ret#))
+
 (defroutes my_routes
   (GET "/me/birth" [] (response (:birth me/me)))
   (GET "/me" [] (response me/me))
   (route/resources "/"))
 
-(def app (wrap-json-response my_routes))
+(def app (bench (wrap-json-response my_routes)))
 
 (defn -main [& args] (run-jetty app {:port 9001}))
